15ec1e78b3bdd7fe400053bd3ad9ed130ae84a0a
Insa Belter <insa.belter@atos.net>
2022-07-12 11:06:57 +0200

    first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TestModule, Test2Module } from 'component-library';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TestModule,
+    Test2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
