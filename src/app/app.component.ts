6f10d355437dd5f91b3deb9885f8d5d17c8f72df
Insa Belter <insa.belter@atos.net>
2022-08-17 10:54:05 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'website-prototype-angular';
-  tags: String[] = ['Elephant', 'Giraffe'];
+  tags: String[] = ['Mehl', 'Eier', 'Birnen'];
 
+  sortTags() {
+    var sortedTags = [...new Set(this.tags)].sort();
+    return sortedTags;
+  }
 }
